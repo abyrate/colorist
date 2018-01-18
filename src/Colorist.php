06da2b5f712ab47e7ed627dd7b7ced3a0257ad3f
boldyrev.a <boldyrev.a@studio2i.ru>
2018-01-18 11:49:50 +0300

    Added a workpiece for the object creation method

--- src/Colorist.php
+++ src/Colorist.php
@@ -3,7 +3,30 @@
 namespace Abyrate;
 
 
+/**
+ * Class Colorist
+ * @package Abyrate
+ */
 class Colorist
 {
+	/**
+	 * Colorist constructor.
+	 *
+	 * @param string $color
+	 *
+	 * @return Colorist
+	 */
+	public function __construct(string $color) {
+		return $this;
+	}
 
+
+	/**
+	 * @param string $color
+	 *
+	 * @return Colorist
+	 */
+	public static function create(string $color) {
+		return new self($color);
+	}
 }
---
+++ tests/CreateObjectTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Abyrate\Colorist;
+
+class CreateObjectTest extends TestCase
+{
+
+
+	public function testCreateObjectFromConstructMethod() {
+		$this->assertTrue((new Colorist('rgb(0,0,0)')) instanceof Colorist);
+	}
+
+
+	public function testCreateObjectFromStaticMethod() {
+		$this->assertTrue(Colorist::create('rgb(0,0,0)') instanceof Colorist);
+	}
+}
