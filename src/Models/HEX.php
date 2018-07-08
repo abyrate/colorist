1d5cbfa05b97f27c82056d168eed620e3b2e2486
aboldyrev <andreybold@bk.ru>
2018-07-08 18:57:54 +0300

    Created HexTrait and moved a part of the code from the HEX model to it

--- src/Models/HEX.php
+++ src/Models/HEX.php
@@ -5,27 +5,24 @@ namespace Abyrate\Models;
 use Abyrate\Exceptions\HexException;
 use Abyrate\Interfaces\ModelInterface;
 use Abyrate\Model;
+use Abyrate\Traits\HexTrait;
 use \stdClass;
 
 class HEX extends Model implements ModelInterface
 {
-	/** @var string $hexRed */
-	protected $hexRed = 0;
+	use HexTrait;
 
-	/** @var string $hexGreen */
-	protected $hexGreen = 0;
+	/** @var string $hex */
+	protected $hex = '#000000';
 
-	/** @var string $hexBlue */
-	protected $hexBlue = 0;
+	/** @var string $hexa */
+	protected $hexa = '#000000ff';
 
 	/** @var array $channels */
 	protected $channels = [ 'hex', 'hexa' ];
 
 	/** @var array $types */
 	protected $types = [ 'hex', 'hexa' ];
-
-	/** @var int $alpha */
-	protected $alpha = 255;
 
 
 	/**
@@ -34,7 +31,9 @@ class HEX extends Model implements ModelInterface
 	 * @return void
 	 */
 	public function __construct($value = NULL) {
-		$this->set($value);
+		if (isset($value)) {
+			$this->set($value);
+		}
 	}
 
 
@@ -49,43 +48,31 @@ class HEX extends Model implements ModelInterface
 
 
 	/**
-	 * @param string|int $alpha
-	 *
-	 * @return void
-	 */
-	public function syncAlpha($alpha) {
-		if (is_string($alpha)) {
-			$alpha = $this->strToDec($alpha);
-		}
-
-		$this->limitation($alpha, 255);
-
-		$this->alpha = $alpha;
-	}
-
-
-	/**
 	 * @param bool $withAlpha
 	 * @param bool $toString
 	 *
 	 * @return string|stdClass
 	 */
 	public function convertToRgb(bool $withAlpha = false, bool $toString = false) {
-		$return = [
-			'red'   => $this->hexRed,
-			'green' => $this->hexGreen,
-			'blue'  => $this->hexBlue,
-		];
+		$hex = str_replace('#', '', $this->hexa);
+
+		list($red, $green, $blue) = $this->explodeHex($hex);
+
+		$red = intval(hexdec($red));
+		$green = intval(hexdec($green));
+		$blue = intval(hexdec($blue));
+
+		$result = compact('red', 'green', 'blue');
 
 		if ($withAlpha) {
-			$return[ 'alpha' ] = $this->alpha / 255;
+			$result[ 'alpha' ] = $this->alpha;
 		}
 
 		if ($toString) {
-			return implode(',', $return);
+			return implode(',', $result);
 		}
 
-		return (object) $return;
+		return (object) $result;
 	}
 
 
@@ -95,22 +82,8 @@ class HEX extends Model implements ModelInterface
 	 * @return void
 	 */
 	public function convertFromRgb($rgb) {
-		$value = preg_replace('/[^\d\,\.]+/', '', $rgb);
-		$hex = explode(',', $value);
-
-		foreach ($hex as &$item) {
-			if (mb_stripos($item, '.') === false) {
-				$item = dechex(intval($item));
-			} else {
-				$item = dechex(intval($item * 255));
-			}
-
-			if (mb_strlen($item) == 1) {
-				$item = '0' . $item;
-			}
-		}
-
-		$this->set(implode('', $hex));
+		$hex = $this->RgbToHex($rgb);
+		$this->set($hex);
 	}
 
 
@@ -146,22 +119,20 @@ class HEX extends Model implements ModelInterface
 
 
 	/**
-	 * @param string|array|null $value
+	 * @param string|array $value
 	 *
 	 * @return void
 	 */
 	public function set($value) {
-		if (isset($value)) {
-			$channels = $this->parseValue($value);
+		$channels = $this->parseHex($value);
+		$this->hex = '#' .
+			$this->decToHex($channels[ 'red' ]) .
+			$this->decToHex($channels[ 'green' ]) .
+			$this->decToHex($channels[ 'blue' ]);
 
-			$this->hexRed = $channels[ 'red' ];
-			$this->hexGreen = $channels[ 'green' ];
-			$this->hexBlue = $channels[ 'blue' ];
+		$this->hexa = $this->hex . $this->decToHex($channels[ 'alpha' ]);
 
-			if ($channels[ 'alpha' ] != $this->alpha) {
-				$this->syncAlpha($channels[ 'alpha' ]);
-			}
-		}
+		$this->syncAlpha($channels[ 'alpha' ] / 255);
 	}
 
 
@@ -172,121 +143,17 @@ class HEX extends Model implements ModelInterface
 	 * @return string|stdClass
 	 */
 	public function get(bool $withAlpha = false, bool $toString = false) {
-		$return = [
-			'hexRed'   => $this->decToHex($this->hexRed),
-			'hexGreen' => $this->decToHex($this->hexGreen),
-			'hexBlue'  => $this->decToHex($this->hexBlue),
-		];
-
 		if ($withAlpha) {
-			$return[ 'alpha' ] = $this->decToHex($this->alpha);
+			$return = [ 'hexa' => $this->hexa ];
+		} else {
+			$return = [ 'hex' => $this->hex ];
 		}
 
+
 		if ($toString) {
-			return '#' . implode('', $return);
+			return array_shift($return);
 		}
 
 		return (object) $return;
-	}
-
-
-	/**
-	 * @param string|array $value
-	 *
-	 * @return array
-	 */
-	protected function parseValue($value):array {
-
-		if (is_string($value)) {
-			if (mb_stripos($value, '#') === 0) {
-				$hex = mb_substr($value, 1);
-			} elseif (mb_stripos($value, '#') !== false) {
-				throw HexException::invalidValue($value);
-			} else {
-				$hex = $value;
-			}
-
-			list($red, $green, $blue, $alpha) = $this->explodeHex($hex);
-
-			$red = $this->strToDec($red);
-			$green = $this->strToDec($green);
-			$blue = $this->strToDec($blue);
-			$alpha = $this->strToDec($alpha);
-
-			$value = [
-				'red'   => $this->limitation($red, 255),
-				'green' => $this->limitation($green, 255),
-				'blue'  => $this->limitation($blue, 255),
-				'alpha' => $this->limitation($alpha, 255),
-			];
-
-		}
-
-		if (is_array($value)) {
-			return $value;
-		} else {
-			throw HexException::notArray();
-		}
-
-	}
-
-
-	/**
-	 * @param string $value
-	 *
-	 * @return int
-	 * @throws HexException
-	 */
-	protected function strToDec(string $value) {
-		if (mb_strlen($value) == 1) {
-			$value = str_repeat($value, 2);
-			return hexdec('0x' . $value);
-		} elseif (mb_strlen($value) == 2) {
-			return hexdec('0x' . $value);
-		} else {
-			throw HexException::invalidValue($value);
-		}
-	}
-
-
-	protected function decToHex($value) {
-		$result = dechex($value);
-
-		if ($value < 16) {
-			$result = '0' . $result;
-		}
-
-		return mb_strtoupper($result);
-	}
-
-
-	/**
-	 * @param string $value
-	 *
-	 * @return array
-	 * @throws HexException
-	 */
-	protected function explodeHex(string $value) {
-		$start = 0;
-		$length = 2;
-		$result = [];
-
-		if (mb_strlen($value) < 6) {
-			$length = 1;
-		}
-
-		while ($result[] = mb_substr($value, $start, $length)) {
-			$start += $length;
-		}
-
-		$result = array_diff($result, [ '' ]);
-
-		if (count($result) == 3) {
-			$result[] = $this->decToHex($this->alpha);
-		} elseif (count($result) != 4) {
-			throw HexException::invalidValue($value);
-		}
-
-		return $result;
 	}
 }
---
+++ src/Traits/HexTrait.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Abyrate\Traits;
+
+
+use Abyrate\Exceptions\HexException;
+
+trait HexTrait
+{
+	protected function RgbToHex($rgb, bool $withAlpha = true) {
+		$value = preg_replace('/[^\d\,\.]+/', '', $rgb);
+		$hex = explode(',', $value);
+
+		foreach ($hex as &$item) {
+			if (mb_stripos($item, '.') === false) {
+				$item = dechex(intval($item));
+			} else {
+				$item = dechex(intval($item * 255));
+			}
+
+			if (mb_strlen($item) == 1) {
+				$item = '0' . $item;
+			}
+		}
+
+		if (count($hex) == 4 && !$withAlpha) {
+			unset($hex[ 3 ]);
+		}
+
+		return implode('', $hex);
+	}
+
+
+	/**
+	 * @param string $value
+	 *
+	 * @return int
+	 * @throws HexException
+	 */
+	protected function strToDec(string $value) {
+		if (mb_strlen($value) == 1) {
+			$value = str_repeat($value, 2);
+			return hexdec('0x' . $value);
+		} elseif (mb_strlen($value) == 2) {
+			return hexdec('0x' . $value);
+		} else {
+			throw HexException::invalidValue($value);
+		}
+	}
+
+
+	protected function decToHex($value) {
+		$result = dechex($value);
+
+		if ($value < 16) {
+			$result = '0' . $result;
+		}
+
+		return mb_strtoupper($result);
+	}
+
+
+
+	/**
+	 * @param string|array $value
+	 *
+	 * @return array
+	 */
+	protected function parseHex($value):array {
+
+		if (is_string($value)) {
+			if (mb_stripos($value, '#') === 0) {
+				$hex = mb_substr($value, 1);
+			} elseif (mb_stripos($value, '#') !== false) {
+				throw HexException::invalidValue($value);
+			} else {
+				$hex = $value;
+			}
+
+			list($red, $green, $blue, $alpha) = $this->explodeHex($hex);
+
+			$red = $this->strToDec($red);
+			$green = $this->strToDec($green);
+			$blue = $this->strToDec($blue);
+			$alpha = $this->strToDec($alpha);
+
+			$value = [
+				'red'   => $this->limitation($red, 255),
+				'green' => $this->limitation($green, 255),
+				'blue'  => $this->limitation($blue, 255),
+				'alpha' => $this->limitation($alpha, 255),
+			];
+
+		}
+
+		if (is_array($value)) {
+			return $value;
+		} else {
+			throw HexException::notArray();
+		}
+
+	}
+
+
+	/**
+	 * @param string $value
+	 *
+	 * @return array
+	 * @throws HexException
+	 */
+	protected function explodeHex(string $value) {
+		$start = 0;
+		$length = 2;
+		$result = [];
+
+		if (mb_strlen($value) < 6) {
+			$length = 1;
+		}
+
+		while ($result[] = mb_substr($value, $start, $length)) {
+			$start += $length;
+		}
+
+		$result = array_diff($result, [ '' ]);
+
+		if (count($result) == 3) {
+			$result[] = $this->decToHex($this->alpha);
+		} elseif (count($result) != 4) {
+			throw HexException::invalidValue($value);
+		}
+
+		return $result;
+	}
+}
